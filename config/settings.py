e78463036ee00e7903e63b42cd88c47b32ccbbd4
exetch <gassemark@gmail.com>
2023-11-16 20:02:06 +0300

    lesson_count added to course serializer

--- config/settings.py
+++ config/settings.py
@@ -39,11 +39,19 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_filters",
     "rest_framework",
+
     "users",
     "course",
     "lesson",
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- course/serializers.py
+++ course/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
+from lesson.models import Lesson
 from .models import Course
 
+
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
-        fields = "__all__"
+        fields = ['id', 'title', 'description', 'preview', 'lesson_count']
+
+    def get_lesson_count(self, obj):
+        return Lesson.objects.filter(course=obj).count()
