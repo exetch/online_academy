2150c7cdd4be44fadb0d94cb4ff0bb33a187221c
exetch <gassemark@gmail.com>
2023-11-16 13:11:33 +0300

    course model implemented

---
+++ course/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Course(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    preview = models.ImageField(upload_to='course_previews/', verbose_name='Превью')
+    description = models.TextField(verbose_name='Описание')
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+    def __str__(self):
+        return self.title
+
