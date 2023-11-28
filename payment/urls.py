31bf8072ddccf2a916987f63b21f8dc9bdc9cc8a
exetch <gassemark@gmail.com>
2023-11-28 16:41:38 +0300

    payment fixed

--- payment/urls.py
+++ payment/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import PaymentListView, create_checkout_session, retrieve_checkout_session, payment_success, payment_cancel
+from .views import PaymentListView, payment_success, payment_cancel, CreateCheckoutSessionView, \
+    RetrieveCheckoutSessionView
 
 urlpatterns = [
     path('payments/', PaymentListView.as_view(), name='payment-list'),
-    path('create-checkout-session/<int:course_id>/', create_checkout_session, name='create-checkout-session'),
-    path('retrieve-checkout-session/<str:session_id>/', retrieve_checkout_session, name='retrieve-checkout-session'),
+    path('create-checkout-session/<int:course_id>/', CreateCheckoutSessionView.as_view(),
+         name='create-checkout-session'),
+    path('retrieve-checkout-session/<str:session_id>/', RetrieveCheckoutSessionView.as_view(),
+         name='retrieve-checkout-session'),
     path('success/', payment_success, name='payment-success'),
     path('cancel/', payment_cancel, name='payment-cancel')
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,11 +1,15 @@
 import stripe
 from django.conf import settings
 from django.http import JsonResponse, HttpResponse
+from rest_framework.response import Response
+from rest_framework import status
 from django_filters.rest_framework import DjangoFilterBackend
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
 from course.models import Course
 from .models import Payment, PaymentSession
 from .serializers import PaymentSerializer
@@ -22,39 +26,46 @@ class PaymentListView(ListAPIView):
     filterset_fields = ['paid_course', 'paid_lesson', 'payment_method']
     ordering_fields = ['payment_date', 'amount']
 
-@csrf_exempt
-def create_checkout_session(request, course_id):
-    course = Course.objects.get(id=course_id)
-    product, price = create_stripe_product(course)
-    session = stripe.checkout.Session.create(
-        payment_method_types=['card'],
-        line_items=[{
-            'price': price,
-            'quantity': 1,
-        }],
-        mode='payment',
-        success_url=request.build_absolute_uri('/success/'),
-        cancel_url=request.build_absolute_uri('/cancel/'),
-    )
-    payment_session = PaymentSession(
-        course=course,
-        session_id=session.id,
-        url=session.url,
-        status='created'
-    )
-    payment_session.save()
+class CreateCheckoutSessionView(APIView):
 
-    return JsonResponse({'sessionId': session.id, 'url': session.url})
+    def post(self, request, course_id):
+        try:
+            course = Course.objects.get(id=course_id)
+            product, price = create_stripe_product(course)
+            session = stripe.checkout.Session.create(
+                payment_method_types=['card'],
+                line_items=[{
+                    'price': price,
+                    'quantity': 1,
+                }],
+                mode='payment',
+                success_url=request.build_absolute_uri('/success/'),
+                cancel_url=request.build_absolute_uri('/cancel/'),
+            )
+            payment_session = PaymentSession(
+                course=course,
+                session_id=session.id,
+                url=session.url,
+                status='created'
+            )
+            payment_session.save()
 
-def retrieve_checkout_session(request, session_id):
-    try:
-        session = stripe.checkout.Session.retrieve(session_id)
+            return Response(session)
+        except Course.DoesNotExist:
+            return Response({'error': 'Course not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        return JsonResponse(session)
-    except stripe.error.StripeError as e:
-        return JsonResponse({'error': str(e)}, status=400)
-    except Exception as e:
-        return JsonResponse({'error': 'An error occurred'}, status=500)
+class RetrieveCheckoutSessionView(APIView):
+
+    def get(self, request, session_id):
+        try:
+            session = stripe.checkout.Session.retrieve(session_id)
+            return Response(session)
+        except stripe.error.StripeError as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': 'An error occurred'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 def payment_success(request):
     return HttpResponse("Оплата успешно завершена!")
